5960dd8cdc0e2b12349c9cb6950caa1902b50b52
benjamin <benjamin@kakao.com>
2012-07-29 02:49:06 +0900

    first commit

---
+++ OnvifWeb/Controllers/OnvifController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using odm.player;
+
+namespace OnvifWeb.Controllers
+{
+    public class OnvifController : Controller
+    {
+        //
+        // GET: /Onvif/
+
+        public string Index()
+        {
+            //VideoBuffer v = new VideoBuffer(700, 600);
+            //return v.TestString();
+            return "Test";
+        }
+    }
+}
---
+++ OnvifWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnvifWeb.MvcApplication" Language="C#" %>
